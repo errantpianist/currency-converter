2b64410b028f17b5c74323d6cbf5983234cfa087
errantpianist <yuzspanish@gmail.com>
2025-08-14 13:04:10 +0100

    Separate currency to Country map

--- src/components/CurrencyDropdown.tsx
+++ src/components/CurrencyDropdown.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react'
 import Select, { components } from 'react-select'
-// Dynamically import all flag components
 import * as FlagIcons from 'country-flag-icons/react/3x2'
+import currencyToCountry from './currencyToCountry'
 
 interface CurrencyDropdownProps {
   label: string
@@ -13,59 +13,6 @@ interface CurrencyDropdownProps {
   isDark?: boolean
 }
 
-// Helper: map currency code to country code for flag
-const currencyToCountry: Record<string, string> = {
-  // Common mappings
-  USD: 'US',
-  EUR: 'EU',
-  GBP: 'GB',
-  JPY: 'JP',
-  AUD: 'AU',
-  CAD: 'CA',
-  CHF: 'CH',
-  CNY: 'CN',
-  HKD: 'HK',
-  NZD: 'NZ',
-  SEK: 'SE',
-  KRW: 'KR',
-  SGD: 'SG',
-  NOK: 'NO',
-  MXN: 'MX',
-  INR: 'IN',
-  RUB: 'RU',
-  ZAR: 'ZA',
-  TRY: 'TR',
-  BRL: 'BR',
-  TWD: 'TW',
-  DKK: 'DK',
-  PLN: 'PL',
-  THB: 'TH',
-  IDR: 'ID',
-  HUF: 'HU',
-  CZK: 'CZ',
-  ILS: 'IL',
-  CLP: 'CL',
-  PHP: 'PH',
-  AED: 'AE',
-  COP: 'CO',
-  SAR: 'SA',
-  MYR: 'MY',
-  RON: 'RO',
-  // Supranational, precious metals, crypto, regional currencies, fallback
-  XAU: '', // Gold
-  XAG: '', // Silver
-  XPT: '', // Platinum
-  XPD: '', // Palladium
-  BTC: '', // Bitcoin
-  ETH: '', // Ethereum
-  // Regional currency representative flags
-  XAF: 'CM', // Cameroon for Central African CFA franc
-  XOF: 'SN', // Senegal for West African CFA franc
-  XCD: 'LC', // Saint Lucia for East Caribbean dollar
-  XPF: 'PF', // French Polynesia for CFP franc
-  XCG: 'CW', // Curaçao for Caribbean guilder
-  // Add more as needed
-}
 
 function getFlagIcon(currency: string) {
   const countryCode = currencyToCountry[currency] || currency.slice(0, 2)
@@ -74,13 +21,6 @@ function getFlagIcon(currency: string) {
   if (Flag) {
     return <Flag title={currency} style={{ width: 24, height: 16, marginRight: 8, borderRadius: 2, boxShadow: '0 0 2px #aaa' }} />
   }
-  // Fallbacks for special cases
-  if (currency === 'XAU') return <span style={{ marginRight: 8 }}>🥇</span>
-  if (currency === 'XAG') return <span style={{ marginRight: 8 }}>🥈</span>
-  if (currency === 'XPT') return <span style={{ marginRight: 8 }}>⚪️</span>
-  if (currency === 'XPD') return <span style={{ marginRight: 8 }}>⚫️</span>
-  if (currency === 'BTC') return <span style={{ marginRight: 8 }}>₿</span>
-  if (currency === 'ETH') return <span style={{ marginRight: 8 }}>Ξ</span>
   // Default fallback
   return <span style={{ marginRight: 8 }}>🏳️</span>
 }
